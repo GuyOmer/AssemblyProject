d1947f12046b0a59a64d6539b4291cd5fbf7af91
Ckone2000 <ckone3000@gmail.com>
2014-04-15 17:18:44 +0300

    Fixed Wait procedure
    Former interrupt wouldn't work on windows machines.
    So I added the "Halt" procedure which waits 1 second base one system
    time.

--- pacman.asm
+++ pacman.asm
@@ -72,161 +72,193 @@ proc PrintMaze
 ;  * Maze Dimensions: 46x24 Chars
 ;**/
 
-push bp
-mov bp,sp
-
-push ax
-push bx
-push cx
-push si
-
-mov si, -1
-print:
-inc si
-mov al, MazeLine1[si] ;get char to print
-
-cmp MazeLine1[si], '#'
-JE Blue
-cmp MazeLine1[si], '.'
-JE Yellow
-cmp MazeLine1[si], '@'
-JE Orange
-
-mov bl, 00h ;set color black
-JMP ResumePrint
-
-Blue:
-mov bl, 99h           ;set color light blue
-JMP ResumePrint
-
-Yellow:
-mov bl, 0eh           ;set color yellow
-JMP ResumePrint
-
-Orange:
-mov bl, 0ch           ;set color light red
-JMP ResumePrint
-
-ResumePrint:
-mov cx, 1             ;how many times to print
-mov ah, 09h
-int 10h               ;print char
-
-call MoveCursor
-
-cmp MazeLine1[si], '$'    ;print char untill terminator ('$')
-JNE print
-
-pop si
-pop bx
-pop cx
-pop ax
-
-pop bp
-
-ret
+    push bp
+    mov bp,sp
+    
+    push ax
+    push bx
+    push cx
+    push si
+    
+    mov si, -1
+    print:
+        inc si
+        mov al, MazeLine1[si]     ;get char to print
+        
+        cmp MazeLine1[si], '#'
+        JE Blue
+        cmp MazeLine1[si], '.'
+        JE Yellow
+        cmp MazeLine1[si], '@'
+        JE Orange
+        
+        mov bl, 00h               ;set color black
+        JMP ResumePrint
+        
+        Blue:
+            mov bl, 99h           ;set color light blue
+            JMP ResumePrint
+            
+        Yellow:
+            mov bl, 0eh           ;set color yellow
+            JMP ResumePrint
+            
+        Orange:
+            mov bl, 0ch           ;set color light red
+            JMP ResumePrint
+            
+        ResumePrint:
+            mov cx, 1             ;how many times to print
+            mov ah, 09h
+            int 10h               ;print char
+            
+        call MoveCursor
+        
+        cmp MazeLine1[si], '$'    ;print char untill terminator ('$')
+    JNE print
+    
+    pop si
+    pop bx
+    pop cx
+    pop ax
+    
+    pop bp
+    
+    ret
 endp
 
 proc MoveCursor
-push bp
-mov bp,sp
-
-push ax
-push bx
-push cx
-push dx
-
-
-mov ah, 03h
-int 10h             ;get cursor position. uses bx,cx,dx
-
-cmp dl, 54          ;print new line after 55 chars
-JNE NextCol
-
-inc dh          ;new line
-mov dl, 0
-JMP MC
-
-NextCol:
-inc dl          ;move cursor foward
-
-MC:
-mov bh, 0 ;page number
-mov ah, 2
-int 10h   ;set cursor position
-
-pop dx
-pop cx
-pop bx
-pop ax
-
-pop bp
-
-ret
+    push bp
+    mov bp,sp
+    
+    push ax
+    push bx
+    push cx
+    push dx
+    
+    
+    mov ah, 03h
+    int 10h             ;get cursor position. uses bx,cx,dx
+    
+    cmp dl, 54          ;print new line after 55 chars
+    JNE NextCol
+    
+    inc dh              ;new line
+    mov dl, 0
+    JMP MC
+    
+    NextCol:
+        inc dl          ;move cursor foward
+    
+    MC:
+        mov bh, 0       ;page number
+        mov ah, 2
+        int 10h         ;set cursor position
+    
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    
+    pop bp
+    
+    ret
 endp
 
 
 proc GetKeyStroke
-push bp
-mov bp, sp
+    push bp
+    mov bp, sp
+    
+    push cx
+    push dx
+    
+    ;mov ax, 0c00h  ;ah = 0ch, al = 00h, so no input is attempted by int
+    ;int 21h        ;flushes keyboard buffer
+    
+    call Halt
+    
+    mov ah, 01h
+    int 16h        ;read from buffer
+    
+    JZ NoStroke    ; if ZF = 1, no keystroke was entered, required?
+    
+    cmp ah, 48h    ;up key was pressed
+    JE Up
+    
+    cmp ah , 4Dh   ;right key was pressed
+    JE Right
+    
+    cmp ah, 50h    ;down key was pressed
+    JE Down
+    
+    cmp ah, 4bh    ;left key was presses
+    JE Left
+    
+    ;not required, debug and simplifaction only!
+    Up:
+        mov ah, 'u'
+        JMP ContGK
+    
+    Right:
+        mov ah, 'r'
+        JMP ContGK
+    
+    Down:
+        mov ah, 'd'
+        JMP ContGK
+    
+    Left:
+        mov ah, 'l'
+        JMP ContGK
+    
+    ContGK:
+    
+        mov ax, 0c00h  ;ah = 0ch, al = 00h, so no input is attempted by int
+        int 21h        ;flushes keyboard buffer
+        
+    NoStroke:
+    
+    pop dx
+    pop cx
+    
+    pop bp
+    
+    ret
+    
+    endp
 
-push cx
-push dx
+proc Halt
+    
+    push bp
+    mov bp, sp
+    
+    push ax
+    push bx
+    push cx
+    push dx
+    
+    mov ah, 00h
+    int 1ah      ;get system time -> CX:DX, ~18 clicks = 1 second 
+     
+    mov bx, dx   ; keep starting time
+    
+    Counter:
+        int 1ah      ;get system time
+        sub dx, bx
+        cmp dx, 19   ;19 clicks > 1 second
+    JNGE Counter ;if dx !>= 19
 
-;mov ax, 0c00h  ;ah = 0ch, al = 00h, so no input is attempted by int
-;int 21h        ;flushes keyboard buffer
-
-call Halt
-
-mov ah, 01h
-int 16h        ;read from buffer
-
-JZ NoStroke    ; if ZF = 1, no keystroke was entered, required?
-
-cmp ah, 48h    ;up key was pressed
-JE Up
-
-cmp ah , 4Dh   ;right key was pressed
-JE Right
-
-cmp ah, 50h    ;down key was pressed
-JE Down
-
-cmp ah, 4bh    ;left key was presses
-JE Left
-
-;not required, debug and simplifaction only!
-Up:
-mov ah, 'u'
-JMP ContGK
-
-Right:
-mov ah, 'r'
-JMP ContGK
-
-Down:
-mov ah, 'd'
-JMP ContGK
-
-Left:
-mov ah, 'l'
-JMP ContGK
-
-ContGK:
-
-mov ax, 0c00h  ;ah = 0ch, al = 00h, so no input is attempted by int
-int 21h        ;flushes keyboard buffer
-
-NoStroke:
-
-pop dx
-pop cx
-
-pop bp
-
-ret
-
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    
+    pop bp
+    
+    ret   
 endp
+
 ends
 
 end start ; set entry point and stop the assembler.
